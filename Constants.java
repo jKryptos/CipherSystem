ffdab122c78795e7014510ac8d93b22079b1db92
John <147920988+jKryptos@users.noreply.github.com>
2026-02-23 05:25:07 -0400

    Delete Constants.java

--- Constants.java
+++
@@ -1,9 +0,0 @@
-public class Constants {
-
-    //For User input of integers
-    static final int DEFAULT_AMOUNT_ZERO = 0;
-
-    //Amount of newlines after menus
-    static final int AMOUNT_OF_NEW_LINES = 1;
-
-}
